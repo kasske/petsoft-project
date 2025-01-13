d96ac6263e29bde67afb82cfbf3c4006941329c1
Kastriot Limani <kasskelimani@gmail.com>
2025-01-13 17:17:56 +0100

    homepage styling, installing shadcn button

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ## Folder structure
 
 Folders with round brackets are for organisational only. The app folder inside app, is used for private pages that need authentication
+
+### Ui
+
+It uses shadcn ui 0.8.0 [shadcn](https://ui.shadcn.com/)
+If you want to add more components from this library please specify version.
+Example of initialization and adding a button:
+
+```bash
+npx shadcn-ui@0.8.0 init
+
+npx shadcn-ui@0.8.0 add button
+```
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,40 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+import previewImage from "../../assets/petsoft-preview.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen flex items-center justify-center gap-10 flex-col xl:flex-row ">
+      <Image
+        src={previewImage}
+        alt="Preview of PetSoft"
+        width={519}
+        height={472}
+      />
+
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime
+          access for $299
+        </p>
+
+        <div className="mt-10 space-x-3">
+          {/* shadcn buttons */}
+          <Button asChild>
+            <Link href="/signup">Sign up</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log in</Link>
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
