d29a902e887a30b7dc30a81c5d37a6e0d1fe0b68
Kastriot Limani <kasskelimani@gmail.com>
2025-01-14 15:02:16 +0100

    app (private) layout and branding, also header and footer

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import Branding from "@/components/branding";
+import H1 from "@/components/h1";
+import Stats from "@/components/stats";
+
 export default function Page() {
-  return <div>Dashboard page</div>;
+  return (
+    <main>
+      <div className="flex justify-between items-center text-white py-8">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
---
+++ src/app/(app)/app/layout.tsx
@@ -0,0 +1,19 @@
+import AppFooter from "@/components/app-footer";
+import AppHeader from "@/components/app-header";
+import BackgroundPattern from "@/components/background-header";
+
+/* layout that works for app route and nested routes inside */
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <BackgroundPattern />
+
+      <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
+        <AppHeader />
+        {children}
+        <AppFooter />
+      </div>
+    </>
+  );
+}
