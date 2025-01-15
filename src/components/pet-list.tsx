36b25c969b620267b95732b1e23ceb5778fc8a9a
Kastriot Limani <kasskelimani@gmail.com>
2025-01-15 10:38:27 +0100

    petlist structure and style

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import placeholderImg from "../assets/pet-placeholder.png";
+
 export default function PetList() {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       <li>
-        <button>Benjamin</button>
+        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src={placeholderImg}
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
       </li>
     </ul>
   );
